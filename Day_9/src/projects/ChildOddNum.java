c8470c52f2b87323a49369dfec82f29df16cb5f7
mnimmz <nimmons92@gmail.com>
2017-07-04 18:05:01 -0400

    Day 9 homework

---
+++ Day_9/src/projects/ChildOddNum.java
@@ -0,0 +1,12 @@
+package projects;
+
+public class ChildOddNum extends DisplayOddNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ChildOddNum oddnumbers = new ChildOddNum();
+		oddnumbers.odd();
+	}
+
+}
---
+++ Day_9/src/projects/DisplayOddNum.java
@@ -0,0 +1,21 @@
+package projects;
+
+public class DisplayOddNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void odd(){
+		
+		int x = -1;
+		
+		while(x < 39){
+			
+			x = x + 2;
+			System.out.println(x);
+		}
+	}
+
+}
